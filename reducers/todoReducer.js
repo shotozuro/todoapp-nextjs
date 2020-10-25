f6fe982b0f9e02d8360509d019eebda2e94b4bbd
Fajri <saladin.elfajr@gmail.com>
2020-10-25 22:52:56 +0700

    add filter feature

--- reducers/todoReducer.js
+++ reducers/todoReducer.js
@@ -1,23 +1,25 @@
 export const todoReducer = (state, action) => {
   switch (action?.type) {
-    case 'ADD_TASK': {
+    case "ADD_TASK": {
       // const trimTask = action.payload.task?.trim();
       // if (trimTask) {
       return {
         ...state,
         todos: [...state.todos, action.payload],
+        allTodos: [...state.todos, action.payload],
         nextId: state.nextId + 1,
         // };
       };
       return state;
     }
-    case 'REMOVE_TASK':
+    case "REMOVE_TASK":
       return {
         ...state,
         todos: state.todos.filter((todo) => todo.id !== action.id),
+        allTodos: state.todos.filter((todo) => todo.id !== action.id),
       };
 
-    case 'EDIT_TASK': {
+    case "EDIT_TASK": {
       const index = state.todos.findIndex((todo) => todo.id === action.id);
       // if (index > -1) {
       //   const trimTask = action.payload.task?.trim();
@@ -29,6 +31,11 @@ export const todoReducer = (state, action) => {
           action.payload,
           ...state.todos.slice(index + 1, state.todos.length),
         ],
+        allTodos: [
+          ...state.todos.slice(0, index),
+          action.payload,
+          ...state.todos.slice(index + 1, state.todos.length),
+        ],
       };
       //   }
       //   return state;
@@ -36,7 +43,7 @@ export const todoReducer = (state, action) => {
       // return state;
     }
 
-    case 'TOGGLE_COMPLETE': {
+    case "TOGGLE_COMPLETE": {
       const index = state.todos.findIndex((todo) => todo.id === action.id);
       // if (index > -1) {
       const selectedTodo = { ...state.todos[index] };
@@ -51,10 +58,29 @@ export const todoReducer = (state, action) => {
           changedStatusTodo,
           ...state.todos.slice(index + 1, state.todos.length),
         ],
+        allTodos: [
+          ...state.todos.slice(0, index),
+          changedStatusTodo,
+          ...state.todos.slice(index + 1, state.todos.length),
+        ],
       };
       // }
       // return state;
     }
+
+    case "FILTER_TASK":
+      return {
+        ...state,
+        todos: state.allTodos.filter((todo) =>
+          todo.task.replace(" ", "").includes(action.payload)
+        ),
+      };
+
+    case "REFRESH_TODO":
+      return {
+        ...state,
+        todos: state.allTodos,
+      };
 
     default:
       return state;
